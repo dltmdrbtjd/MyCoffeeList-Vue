414a9ca45f2e3462965073bb0d1362e5c34fa916
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-11-13 03:18:57 +0900

    vuex loadList func

--- FE/src/store/index.js
+++ FE/src/store/index.js
@@ -1,12 +1,29 @@
+import axios from 'axios'
 import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    coffeeList: []
+  },
+  getters: {
+    getCoffee: (state) => (id) => {
+      return state.coffeeList.find((coffee) => coffee.id === id)
+    }
   },
   mutations: {
+    loadCoffeeList(state, list) {
+      const coffee = list || []
+      state.coffeeList.push(coffee)
+    }
   },
   actions: {
-  },
-  modules: {
+    async loadCoffeeList({ commit }) {
+      const res = await axios.get('http://localhost:5000')
+      commit('loadCoffeeList', res.data)
+    },
+
+    pushCoffee({ commit }, coffee) {
+      commit('loadCoffeeList', coffee)
+    }
   }
 })
